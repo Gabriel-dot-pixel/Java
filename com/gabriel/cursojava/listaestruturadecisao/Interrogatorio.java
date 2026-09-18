66f53b8ef351c3c86c7401998f8c25e04ba9448d
Gabriel-dot-pixel <marinho.gabriel@aluno.ifsp.edu.br>
2026-09-18 18:27:00 -0300

    fiz uma classe

---
+++ com/gabriel/cursojava/listaestruturadecisao/Interrogatorio.java
@@ -0,0 +1,13 @@
+package com.gabriel.cursojava.listaestruturadecisao;
+
+import java.util.Scanner;
+
+public class Interrogatorio {
+
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+
+        
+    }
+}
